24a8f4f1b9fc1469357f0284fb7decdf1e8e91b2
josejo48-commits <josejo48@gmail.com>
2026-07-29 11:09:47 -0500

    Add files via upload

--- sw_racimos.js
+++ sw_racimos.js
@@ -1,11 +1,24 @@
-const CACHE = 'racimos-v2';
-const FILES = ['/potencial-aceite/muestreo_racimos.html'];
+const CACHE = 'racimos-v3';
+const FILES = [
+  '/potencial-aceite/muestreo_racimos.html',
+  '/potencial-aceite/manifest_racimos.json'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
-self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => clients.claim())
+  );
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
